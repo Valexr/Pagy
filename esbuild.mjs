57030354d081a086e4570f361bf444aefe2b14ab
Valexr <valexr@gmail.com>
2022-04-21 11:07:55 +0300

    updeps

--- esbuild.mjs
+++ esbuild.mjs
@@ -65,7 +65,7 @@ async function build_server() {
         legalComments: 'none',
         plugins: [
             // envPlugin(),
-            plugin_server(),
+            serverPlugin(),
             eslintPlugin()
         ]
     });
@@ -89,7 +89,7 @@ async function build_client() {
     });
 }
 
-function plugin_server() {
+function serverPlugin() {
     return {
         name: 'server-plugin',
         setup(b) {
@@ -138,6 +138,28 @@ function plugin_server() {
                 };
             });
         }
+    };
+}
+
+function envPlugin() {
+    return {
+        name: 'env',
+        setup(build) {
+            // Intercept import paths called "env" so esbuild doesn't attempt
+            // to map them to a file system location. Tag them with the "env-ns"
+            // namespace to reserve them for this plugin.
+            build.onResolve({ filter: /^env$/ }, args => ({
+                path: args.path,
+                namespace: 'env-ns',
+            }));
+
+            // Load paths tagged with the "env-ns" namespace and behave as if
+            // they point to a JSON file containing the environment variables.
+            build.onLoad({ filter: /.*/, namespace: 'env-ns' }, () => ({
+                contents: JSON.stringify(process.env),
+                loader: 'json',
+            }));
+        },
     };
 }
 
